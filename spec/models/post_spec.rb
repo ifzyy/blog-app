e06e74985c1fc0a42520243e1319c001def6e98c
ifzyy <johnsonnifemi8@gmail.com>
2022-10-21 21:47:45 +0100

    fixed reviewers changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,6 +14,15 @@ RSpec.describe Post, type: :model do
                comments_counter: 3, likes_counter: 'two')
       expect(subject).to_not be_valid
     end
+
+    it 'should not accept more than 250 character' do
+      subject.title = "
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+      eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+      montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+      eu, pretium quis,"
+      expect(subject).to_not be_valid
+    end
   end
 
   describe 'Method Update post counter' do
@@ -24,6 +33,9 @@ RSpec.describe Post, type: :model do
       post.update_posts_counter
       expect(user.posts_counter).to eq(4)
     end
+    it 'post_counter should be a greater than or equal to 0' do
+      expect(user.posts_counter).to be >= 0
+    end
   end
 
   describe 'Method recent comment should return latest 5 comments' do
@@ -32,6 +44,20 @@ RSpec.describe Post, type: :model do
     before { 6.times { |n| Comment.create(post_id: post.id, author_id: user.id, text: "Hi comment #{n}") } }
     it 'should return 5 recent comments' do
       expect(post.recent_comments).to eq(post.comments.last(5))
+    end
+  end
+
+  describe 'get comments counter and like counter' do
+    post = Post.create(author_id: user.id, title: 'Full stack Tutorial', text: 'This is complete tutorial',
+                       comments_counter: 3, likes_counter: 2)
+    before { 6.times { |n| Comment.create(post_id: post.id, author_id: user.id, text: "Hi comment #{n}") } }
+
+    it 'likes_counter should be a greater than or equal to 0' do
+      expect(post.likes_counter).to be >= 0
+    end
+
+    it 'comments_counter should be a greater than or equal to 0' do
+      expect(post.comments_counter).to be >= 0
     end
   end
 end
